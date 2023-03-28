e077b9cbc47350add09b1d8546de777cc435fd3e
The_Baum <102868970+TheBaum123@users.noreply.github.com>
2023-03-28 23:21:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+let Todos = { }
 
+if(localStorage.getItem("todos") != null) {
+    Todos = JSON.parse(localStorage.getItem("todos"))
+    console.log(Todos)
+}
+
+for(let i = 0; i < Object.keys(Todos).length; i++) {
+    const currentTodoUUID = (Object.keys(Todos)[i])
+    const currentTodo = (Todos[currentTodoUUID])
+    console.log(currentTodo + " is stored in " + currentTodoUUID)
+}
+
+function addTodo() {
+    const input = document.getElementById("input").value
+    const uuid = crypto.randomUUID()
+    Todos = Object.assign(Todos, {[uuid]:input})
+    console.log(Todos)
+    localStorage.setItem("todos", JSON.stringify(Todos))
+}
