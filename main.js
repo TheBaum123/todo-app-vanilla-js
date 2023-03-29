b241a0a221bda13409bc6add500b9161126c2133
The_Baum123 <the.baum12345@gmail.com>
2023-03-29 13:04:15 +0200

    can now list todos

--- main.js
+++ main.js
@@ -2,19 +2,46 @@ let Todos = { }
 
 if(localStorage.getItem("todos") != null) {
     Todos = JSON.parse(localStorage.getItem("todos"))
-    console.log(Todos)
 }
 
 for(let i = 0; i < Object.keys(Todos).length; i++) {
     const currentTodoUUID = (Object.keys(Todos)[i])
     const currentTodo = (Todos[currentTodoUUID])
     console.log(currentTodo + " is stored in " + currentTodoUUID)
+    const todoDiv = document.createElement("div")
+    const textNode = document.createTextNode(currentTodo)
+    todoDiv.appendChild(textNode)
+    document.getElementById("todos").appendChild(todoDiv)
 }
 
 function addTodo() {
     const input = document.getElementById("input").value
     const uuid = crypto.randomUUID()
-    Todos = Object.assign(Todos, {[uuid]:input})
-    console.log(Todos)
-    localStorage.setItem("todos", JSON.stringify(Todos))
+    if(input != "") {
+        Todos = Object.assign(Todos, {[uuid]:input})
+        localStorage.setItem("todos", JSON.stringify(Todos))
+    }
+    refresh()
+}
+
+function refresh() {
+    if(localStorage.getItem("todos") != null) {
+        Todos = JSON.parse(localStorage.getItem("todos"))
+    }
+    for(let i = 0; i < Object.keys(Todos).length; i++) {
+        const currentTodoUUID = (Object.keys(Todos)[i])
+        const currentTodo = (Todos[currentTodoUUID])
+        console.log(currentTodo + " is stored in " + currentTodoUUID)
+    }
+    window.location.reload()
+}
+
+document.onkeypress = function(e) {
+  if(document.activeElement.id != "input") {
+    document.getElementById("input").value = document.getElementById("input").value + e.key
+    document.getElementById("input").focus()
+  }
+  if(e.key == "Enter") {
+    addTodo()
+  }
 }
